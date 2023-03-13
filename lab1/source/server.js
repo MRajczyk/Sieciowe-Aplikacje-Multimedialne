2d0026270e824e97ab0b1740587128d7c63e7306
Mikołaj Rajczyk <103463343+MRajczyk@users.noreply.github.com>
2023-03-13 15:11:35 +0100

    Added audio functionality

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -4,13 +4,19 @@ const app = express()
 
 app.get('/', (req, res) => {
     videoUrl = req.query.videoFile;
-      
+    audioUrl = req.query.audioFile;
+    
     output = "";
       
     if(videoUrl !== undefined) {
     output += (`<video width=\"320\" height=\"240\" id=\"videoPlayer\" controls><source src=${videoUrl}>You can't run video tag</video>`);
     }
+    if(audioUrl !== undefined) {
+            output += (`<audio width=\"320\" height=\"240\" id=\"audioPlayer\" controls><source src=${audioUrl}>You can't run audio tag</audio>`);
+    }
+      
     res.set('Content-Type', 'text/html', 'charset', 'utf-8');
     res.send(output);
 })
+
 app.listen(4080)

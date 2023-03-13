ffeb0d85b8bc465af09a660e8f7a0ea2579e817d
Mikołaj Rajczyk <103463343+MRajczyk@users.noreply.github.com>
2023-03-13 15:05:39 +0100

    videoPlayer functionality added

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -3,7 +3,14 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    videoUrl = req.query.videoFile;
+      
+    output = "";
+      
+    if(videoUrl !== undefined) {
+    output += (`<video width=\"320\" height=\"240\" id=\"videoPlayer\" controls><source src=${videoUrl}>You can't run video tag</video>`);
+    }
+    res.set('Content-Type', 'text/html', 'charset', 'utf-8');
+    res.send(output);
 })
-
 app.listen(4080)
